63c5042c347cd35233c53f1583277cb09d79db40
Robert A. Uhl <bob.denver.co@gmail.com>
2016-07-17 18:22:44 -0600

    Complete documenting the desktop-file functions

--- package.lisp
+++ package.lisp
@@ -4,16 +4,18 @@
 (defpackage #:cl-desktop
   (:use #:cl)
   (:export #:parse-desktop-file
+           #:load-desktop-files
+
            #:get-string-key
            #:get-strings-key
            #:get-locale-string-key
            #:get-locale-strings-key
            #:get-boolean-key
            #:get-number-key
+
            #:*xdg-data-home*
            #:*xdg-config-home*
            #:*xdg-data-dirs*
            #:*xdg-config-dirs*
            #:*xdg-cache-home*
-           #:*xdg-runtime-dir*
-           #:load-desktop-files))
+           #:*xdg-runtime-dir*))
